7bde6aa74f8347c050d1e33cc41c4443161123f9
Casey L <casey.loria@hotmail.com>
2019-01-26 09:05:58 -0500

    Loneliness

---
+++ GGJTeam2/Assets/Test Folder/Loneliness Test/LonelinessScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LonelinessScript : MonoBehaviour
+{
+    public GameObject home;
+
+    public float distanceFromHome;
+
+    public bool lonely;
+
+    public Text lonelyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        home = FindObjectOfType<Home>().gameObject;
+        lonelyText = GameObject.Find("LonelyText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerDistance();
+    }
+
+    void FixedUpdate()
+    {
+        if (lonely)
+        {
+            lonelyText.text = "Lonely";
+            lonelyText.color = Color.blue;
+        }
+        else
+        {
+            lonelyText.text = "Happy";
+            lonelyText.color = Color.green;
+        }
+
+    }
+
+    void GetPlayerDistance()
+    {
+        distanceFromHome = Vector2.Distance(this.transform.position,home.transform.position);
+
+        if (distanceFromHome > 6)
+        {
+            lonely = true;
+        }
+        else
+            lonely = false;
+    }
+}
